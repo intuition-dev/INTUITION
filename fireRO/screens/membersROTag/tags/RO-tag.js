35816420fdbd93da00366adc00b0ac504a5975d7
unknown <uptimevic@gmail.com>
2018-08-24 18:41:56 -0400

    no data

--- fireRO/screens/membersROTag/tags/RO-tag.js
+++ fireRO/screens/membersROTag/tags/RO-tag.js
@@ -1,9 +1,9 @@
 
-riot.tag2('ro-tag', '<form class="grid-form" id="f2" autocomplete="off"> <fieldset> <legend>RiotJS Tag</legend> <div data-row-span="2"> <div data-field-span="1"> <label> Data</label> <input type="text" name="dat" placeholder="more data"> </div> </div> </fieldset> </form> <div class="buts"> <button class="btn" id="data" type="submit">Add data</button> </div>', 'ro-tag .buts button,[data-is="ro-tag"] .buts button{ margin: .5rem; }', '', function(opts) {
+riot.tag2('ro-tag', '<hr> <p>{dat}</p> <hr> <div class="buts"> <button class="btn" id="data" type="submit">Get data</button> </div>', 'ro-tag .buts button,[data-is="ro-tag"] .buts button{ margin: .5rem; }', '', function(opts) {
     const fconfig = {
     	apiKey: 'AIzaSyASZVGB4'+'s'+'1uKB4OdxPCcbJ3ebf44KUYdIE',
-    	authDomain: 'mbake1-1.firebaseapp.com',
-    	projectId: 'mbake1-1'
+    	authDomain: 'nbake1-1.firebaseapp.com',
+    	projectId: 'nbake1-1'
     }
 
     firebase.initializeApp(fconfig)
@@ -13,19 +13,25 @@ riot.tag2('ro-tag', '<form class="grid-form" id="f2" autocomplete="off"> <fields
     db.settings(dsettings)
 
     console.log('oh hi')
-    loadjs.ready('style', function () {
+    thiz = this
+    this.on('mount', function(){
+
+       loadjs.ready('style', function () {
+       let tableOneRef = db.collection('table_one')
 
        $('#data').click(function(){
-          let data = $('#f2 [name="dat"]').val()
-          console.log(data)
+          console.log('click')
 
-          newPK.set(newRow)
-             .then(function() {
-                console.log('successful')
+          tableOneRef.get().then(function(querySnapshot) {
+             console.log('back')
+             querySnapshot.forEach(function(doc) {
+                console.log(doc.id, " => ", doc.data());
+                thiz.update({dat: doc.data()})
+                })
              })
-             .catch(function(error) {
-                console.error('oops', error)
-             })
+
+          })
+
        })
 
     })
