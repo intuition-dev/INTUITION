ac6ee03577b7354df92d7d21aa5ffb2a7e52e999
unknown <uptimevic@gmail.com>
2018-08-27 18:35:52 -0400

    lower

--- src/x.cmd
+++ src/x.cmd
@@ -1,8 +1,8 @@
 cls
 call tsc
-rem ts-node mbake.ts /Users/uptim/Documents/GitHub/examples-plugins/navSiteS/ex
+rem ts-node mbake.ts -f /Users/uptim/Documents/GitHub/examples-plugins/webSiteS
 ts-node mbake.ts
 rem ts-node mbake.ts -i /Users/uptim/Documents/GitHub/examples-plugins/navSite/team
 rem ts-node mbake.ts /Users/uptim/Documents/GitHub/examples-plugins/blog
-rem ts-node mbake.ts -t \Users\uptim\Documents\GitHub\_mBake\CRUDA\riotFirst
+rem ts-node mbake.ts -t \Users\uptim\Documents\GitHub\_mBake\fire-ro\riotFirst
 rem ts-node mbake.ts -j .
