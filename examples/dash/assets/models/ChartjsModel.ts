168c4f115112d3bf24dab7ddd169400479bdb5de
Liza K <liza.kislyakova@gmail.com>
2019-01-09 14:24:55 +0300

    bubble chartjs done on model-view

--- examples/dash/assets/models/ChartjsModel.ts
+++ examples/dash/assets/models/ChartjsModel.ts
@@ -24,9 +24,47 @@ class ChartjsModel {
             bgColor: '#c45850',
             data: 433
         }];
+
+        let data2 = [{
+            label: 'China',
+            bg: 'rgba(255,221,50,0.2)',
+            border: 'rgba(255,221,50,1)',
+            data: {
+                x: 21269017,
+                y: 5.245,
+                r: 15
+            }
+        },{
+            label: 'Denmark',
+            bg: 'rgba(60,186,159,0.2)',
+            border: 'rgba(60,186,159,1)',
+            data: {
+                x: 258702,
+                y: 7.526,
+                r: 10
+            }
+        },{
+            label: 'Germany',
+            bg: 'rgba(0,0,0,0.2)',
+            border: '#000',
+            data: {
+                x: 3979083,
+                y: 6.994,
+                r: 15
+            }
+        },{
+            label: 'Japan',
+            bg: 'rgba(193,46,12,0.2)',
+            border: 'rgba(193,46,12,1)',
+            data: {
+                x: 4931877,
+                y: 5.921,
+                r: 15
+            }
+        }];
         
         if(this.dataSourceType == 'fake'){
-            return data;
+            return [data, data2];
         }
 
 
--- examples/dash/screen/chartjs/ChartjsBind.js
+++ examples/dash/screen/chartjs/ChartjsBind.js
@@ -11,21 +11,21 @@ class ChartjsBind {
       if( this._data !=null){
 
          let chartData = this._data.read();
-         // console.log('chartData', chartData);
+         // console.log('chartData', chartData[0]);
          let label = [];
          let bgColor = [];
          let coord = [];
 
-         for (let i = 0; i < chartData.length; i++) {
-            label.push(chartData[i].label);
+         for (let i = 0; i < chartData[0].length; i++) {
+            label.push(chartData[0][i].label);
          }
 
-         for (let i = 0; i < chartData.length; i++) {
-            bgColor.push(chartData[i].bgColor);
+         for (let i = 0; i < chartData[0].length; i++) {
+            bgColor.push(chartData[0][i].bgColor);
          }
 
-         for (let i = 0; i < chartData.length; i++) {
-            coord.push(chartData[i].data);
+         for (let i = 0; i < chartData[0].length; i++) {
+            coord.push(chartData[0][i].data);
          }
 
          // console.log('label', label);
@@ -66,6 +66,87 @@ class ChartjsBind {
                }
             }
             initC(canvasId, config);
+         });
+      }
+   }
+
+   init2(canvasId){
+      
+      this.canvas = '#'+canvasId;
+      if( this._data !=null){
+
+         let chartData = this._data.read();
+         // console.log('chartData', chartData[1]);
+         let label = [];
+         let bg = [];
+         let border = [];
+         let coord = [];
+
+         for (let i = 0; i < chartData[1].length; i++) {
+            label.push(chartData[1][i].label);
+         }
+
+         for (let i = 0; i < chartData[1].length; i++) {
+            bg.push(chartData[1][i].bg);
+         }
+
+         for (let i = 0; i < chartData[1].length; i++) {
+            border.push(chartData[1][i].border);
+         }
+
+         for (let i = 0; i < chartData[1].length; i++) {
+            coord.push(chartData[1][i].data);
+         }
+
+         console.log('label', label);
+         console.log('bgColor', bg);
+         console.log('border', border);
+         console.log('coord', coord);
+
+         depp.require(['chartjs'], function(){
+
+            config = {
+               type: 'bubble',
+               data: {
+                  datasets: [{
+                     label: label,
+                     backgroundColor: bg,
+                     borderColor: border,
+                     data: coord
+                  }]
+               },
+   
+               options: {
+                  responsive: false,
+                  layout: {
+                     padding: 30
+                  },
+                  maintainAspectRatio: false,
+                  animation: {
+                     duration: 350
+                  },
+                  title: {
+                     display: true,
+                     text: 'Predicted world population (millions) in 2050'
+                  },
+                  scales: {
+                     yAxes: [{
+                        scaleLabel: {
+                           display: true,
+                           labelString: 'Happiness'
+                        }
+                     }],
+                     xAxes: [{
+                        scaleLabel: {
+                           display: true,
+                           labelString: 'GDP (PPP)'
+                        }
+                     }]
+                  }
+               }
+            }
+            initC(canvasId, config);
+
          });
       }
    }
