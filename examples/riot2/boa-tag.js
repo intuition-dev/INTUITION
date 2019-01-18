6890d1563084e10118f2033d3e640646dde4d355
Liza K <liza.kislyakova@gmail.com>
2019-01-18 20:56:55 +0300

    rollback

--- examples/riot2/boa-tag.js
+++ examples/riot2/boa-tag.js
@@ -1,12 +1,5 @@
 
-riot.tag2('boa-tag', '<p>A simple example of calculating. The form below is passed inside a tag using Yield feature</p> <p class="num">{num}</p><yield></yield> <button type="submit">Check sum!</button>', '', '', function(opts) {
-    $(document).on('click', 'button', e => {
-        e.preventDefault();
-        let arg1 = $('input#one').val();
-        let arg2 = $('input#two').val();
-        boaTag.doSomething(arg1, arg2);
-    });
-
+riot.tag2('boa-tag', '<p>A simple example of calculating. The form below is passed inside a tag using Yield feature</p> <p class="num">{num}</p><yield></yield>', '', '', function(opts) {
     this.doSomething = function(arg1, arg2) {
         this.update({num: +arg1+ +arg2});
     }.bind(this)
