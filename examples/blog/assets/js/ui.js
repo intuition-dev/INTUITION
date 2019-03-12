6697e55889e180b91e854d261e44771a609ab1a6
Liza K <liza.kislyakova@gmail.com>
2019-03-12 19:49:09 +0300

    [blog]: search styles update

--- examples/blog/assets/js/ui.js
+++ examples/blog/assets/js/ui.js
@@ -6,3 +6,11 @@ if ($('[data-js-menu-highlight="about"]').length !== 0) {
 } else if ($('[data-js-menu-highlight="forum"]').length !== 0) {
     $('header a.forum').addClass('active');
 }
+
+$('.js-srch-input').focus(function() {
+    $(this).parents('form').find('.srch-btn').addClass('focus');
+});
+
+$('.js-srch-input').focusout(function() {
+    $(this).parents('form').find('.srch-btn').removeClass('focus');
+});
