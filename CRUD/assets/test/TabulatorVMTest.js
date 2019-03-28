6eeb044367f1d02463e576177c2ada76a1cb704a
Natalia Melikyants <melikyants@gmail.com>
2019-03-28 16:22:00 +0100

    test

--- CRUD/assets/test/TabulatorVMTest.js
+++ CRUD/assets/test/TabulatorVMTest.js
@@ -1,9 +1,14 @@
-QUnit.test('got the data for the view', function (assert) {
-   var viewModel = new TabulatorViewModel()
-   var thenable = Promise.all([viewModel.read()])
-      .then(function () {
-         let data = viewModel.getViewList('example-table')
-         assert.ok(data.length, 'there is some data');
-      })
-   return thenable;
-});
+class TabulatorVMTest {
+   init() {
+      QUnit.test('got the data for the view', function (assert) {
+         var viewModel = new TabulatorViewModel()
+         var thenable = Promise.all([viewModel.read()])
+            .then(function () {
+               let data = viewModel.getViewList('example-table')
+               assert.ok(data.length, 'there is some data');
+            })
+         return thenable;
+      });
+   }
+}
+
