7d7f1396aae85035d1934adbe6563e84d6aef8b5
Esperanza González <esperanza@mechaphysis.org>
2019-04-06 14:18:17 +0200

    feat: Define general purpose CRUD functions in FP style

---
+++ CRUD/assets/models/service/utilities/functionalCRUD.ts
@@ -0,0 +1,41 @@
+/*
+==============
+Functional CRUD approach:
+==============
+*/
+
+//These functions we can reuse across all of our services. We also provide a safe default database.
+//So in case we don't specify the db, it will read from default.
+//We may now use them for piping/composing further custom functionality.
+
+
+export const getFromDatabase = (database = db1) => entity => database.collection(entity)
+
+//We avoid the use of ifs, thanks to partial application and ternary operator
+export const read = table => (id?: string) => id ? 
+    table.doc(id).get()
+    .then((docSnap: any) => docSnap.data())
+    .catch( (error: any) => console.log('Woops, something went wrong: ', error) ) :
+
+    table.doc().get()
+    .then( (docSnap: any)=> docSnap.data() )
+    .catch( (error: any) => console.log('Woops, something went wrong: ', error) )
+
+
+export const add = table => row => table.doc().set(row)
+    .then(() => console.log('data added succesfully'))
+    .catch((error: any) => console.log('woops, something went wrong', error))
+
+
+export const update = table => row => table.doc(row['id']).set(row)
+        .then( ()=> console.log('data updated succesfully') )
+        .catch( (error: any) => console.log('Woops, something went wrong: ', error) )
+
+
+//naming it remove due to colliding with 'delete' built-in JS operator.
+export const remove = (table: any )=> row =>  table.doc(row['id']).delete()
+                .then( () => console.log('data successfully deleted') )
+                .catch( (error: any) => console.log('Woops, something went wrong: ', error) )
+
+
+
