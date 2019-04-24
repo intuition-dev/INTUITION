2d5a3f1d8ecb6cafd0eadaf88c2a527cc72feec1
Vic <uptimevic@gmail.com>
2019-04-24 18:58:47 -0400

    comments

--- src/publish.sh
+++ src/publish.sh
@@ -5,9 +5,9 @@ tsc
 node mbake.js
 npm publish
 #sudo npm i -g mbake --unsafe-perm=true --allow-root
-#sudo yarn global remove mbake
+# if changed node version do this:
+# sudo yarn global remove mbake
 sudo yarn global add mbake
 mbakeX
-#yarn global upgrade 
 
 # find . -type f -name 'package-lock.json' 
