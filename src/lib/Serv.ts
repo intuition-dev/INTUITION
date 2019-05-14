57d71427e094132bbb34fa6247a5878a2c8aa75e
Vic <uptimevic@gmail.com>
2019-05-14 11:44:11 -0400

    txt

--- src/lib/Serv.ts
+++ src/lib/Serv.ts
@@ -1,7 +1,7 @@
 // All rights reserved by Metabake (Metabake.org) | Cekvenich, licensed under LGPL 3.0
 // NOTE: You can extend these classes!
 
-export class CustomCors { // for use with http-rpc
+export class CustomCors { // will be deprecated , use the class under here: ExpressRPC
    constructor(orig) {
       return (request, response, next) => {
          response.setHeader('Access-Control-Allow-Origin', orig)
@@ -18,6 +18,7 @@ const formidable = require('express-formidable')
 export class ExpressRPC {
    
    static makeInstance(orig) {
+      console.log('Allowed',orig)
       const cors = new CustomCors(orig)
       const appInst = express()
       appInst.use(cors)
