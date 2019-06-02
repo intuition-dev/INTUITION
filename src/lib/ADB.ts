346c925ab31cbe16cf2f56ca70971eff8e7359c1
Vic <uptimevic@gmail.com>
2019-06-02 14:18:35 -0400

    sync

--- src/lib/ADB.ts
+++ src/lib/ADB.ts
@@ -2,9 +2,13 @@
 
 import sqlite from 'sqlite'
 import SQL from 'sql-template-strings';
+import emailjs from 'emailjs-com'
+
+// will need API
 
 // guid for pk client side 
 // eg: bcrypt randomBytes(16).toString("hex") or base64, or Math.random to make base64 char 16 times
+// also to email a random # 
 
 export class ADB {
    // emailjs is client side api
