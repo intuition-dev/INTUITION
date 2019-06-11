447c000b3a97078e9914639b22cceeb7704886bc
Natalia Melikyants <melikyants@gmail.com>
2019-06-11 13:46:17 +0200

    shopitem before comp

--- shop/shopHook/index.js
+++ shop/shopHook/index.js
@@ -22,16 +22,15 @@ app.post('/test', function (req, res) {
         res.json(resp);
     }
 });
-app.post('/printify-rate', function (req, res) {
-    console.info("--res:", res);
-    console.log('--req.fields', req.fields);
+app.post('/printful-rate', function (req, res) {
+    console.info("1)-------printful-rate req:", req.fields);
     const method = req.fields.mode;
     const params = req.fields;
     const resp = {};
     let shippingAddress = params.content['shippingAddress'];
     console.info("--shippingAddress:", shippingAddress);
     let items_g = params.content['items'];
-    console.info("--items:", items_g);
+    console.log("TCL: params", params);
     let temp_shipping = {};
     temp_shipping['name'] = shippingAddress.fullName;
     temp_shipping['address1'] = shippingAddress.address1;
@@ -78,38 +77,35 @@ app.post('/printify-rate', function (req, res) {
         res.json(resp);
     }
 });
-app.post('/printify-create-order', (req, res) => {
-    console.info("--res:", res);
-    console.log(req.fields);
+app.post('/printful-create-order', (req, res) => {
+    console.info("2)-----printful-create-order:");
     const user = req.fields.user;
     const pswd = req.fields.pswd;
     const mode = req.fields.mode;
-    console.info("--mode:", mode);
     const params = req.fields;
-    let shippingAddress = params.content['shippingAddress'];
-    console.info("--shippingAddress:", shippingAddress);
-    let items_g = params.content['items'];
-    console.info("--items:", items_g);
-    let temp_shipping = {};
-    temp_shipping['name'] = shippingAddress.fullName;
-    temp_shipping['address1'] = shippingAddress.address1;
-    temp_shipping['city'] = shippingAddress.city;
-    temp_shipping['country_code'] = shippingAddress.country;
-    temp_shipping['state_code'] = shippingAddress.province;
-    temp_shipping['zip'] = shippingAddress.postalCode;
-    let elements = [];
-    items_g.map(function (item) {
-        let temp = {};
-        temp['quantity'] = item.quantity;
-        temp['sync_variant_id'] = item.metadata.sync_variant_id;
-        elements.push(temp);
-    });
-    let send_order = Object.assign({ recipient: temp_shipping, items: elements });
-    console.info("--send_order:", send_order);
     const resp = {};
-    if ('Test' == mode) {
+    if ('Test' == mode && params.eventName == 'order.completed') {
         resp.type = '';
         resp.ispacked = false;
+        let shippingAddress = params.content['shippingAddress'];
+        console.log("TCL: params", params);
+        let items_g = params.content['items'];
+        let temp_shipping = {};
+        temp_shipping['name'] = shippingAddress.fullName;
+        temp_shipping['address1'] = shippingAddress.address1;
+        temp_shipping['city'] = shippingAddress.city;
+        temp_shipping['country_code'] = shippingAddress.country;
+        temp_shipping['state_code'] = shippingAddress.province;
+        temp_shipping['zip'] = shippingAddress.postalCode;
+        let elements = [];
+        items_g.map(function (item) {
+            let temp = {};
+            temp['quantity'] = item.quantity;
+            temp['sync_variant_id'] = item.metadata.sync_variant_id;
+            elements.push(temp);
+        });
+        let send_order = Object.assign({ recipient: temp_shipping, items: elements });
+        console.info("--send_order:", send_order);
         console.log(resp);
         request({
             url: "https://api.printful.com/orders",
--- shop/shopHook/index.ts
+++ shop/shopHook/index.ts
@@ -12,158 +12,156 @@ const app = ExpressRPC.makeInstance(config.cors);
 const port = 3000
 
 app.post('/test', function (req, res) {
-    const method = req.fields.method
-    console.info("--method:", method)
-    const params = JSON.parse(req.fields.params)
-    console.info("--params:", params)
-    const resp: any = {} // new response that will be set via the specific method passed
+   const method = req.fields.method
+   console.info("--method:", method)
+   const params = JSON.parse(req.fields.params)
+   console.info("--params:", params)
+   const resp: any = {} // new response that will be set via the specific method passed
 
-    if ('test-connection' == method) {
-    } else {
-        resp.errorLevel = -1
-        resp.errorMessage = 'mismatch'
-        console.log('noway', resp)
-        res.json(resp)
-    }
+   if ('test-connection' == method) {
+   } else {
+      resp.errorLevel = -1
+      resp.errorMessage = 'mismatch'
+      console.log('noway', resp)
+      res.json(resp)
+   }
 
 })
 
-app.post('/printify-rate', function (req, res) {
-    console.info("--res:", res)
-    console.log('--req.fields', req.fields)
-    // const user = req.fields.user
-    // const pswd = req.fields.pswd // just changed from pswdH
+app.post('/printful-rate', function (req, res) {
+   console.info("1)-------printful-rate req:", req.fields)
 
-    const method = req.fields.mode
-    // const params = JSON.parse(req.fields)
-    const params = req.fields
+   const method = req.fields.mode
+   // const params = JSON.parse(req.fields)
+   const params = req.fields
 
-    const resp: any = {} // new response
-    let shippingAddress = params.content['shippingAddress']
-    console.info("--shippingAddress:", shippingAddress)
-    let items_g = params.content['items']
-    console.info("--items:", items_g)
+   const resp: any = {} // new response
+   let shippingAddress = params.content['shippingAddress']
+   console.info("--shippingAddress:", shippingAddress)
+   let items_g = params.content['items']
+   console.log("TCL: params", params)
 
-    let temp_shipping = {}
-    temp_shipping['name'] = shippingAddress.fullName
-    temp_shipping['address1'] = shippingAddress.address1
-    temp_shipping['city'] = shippingAddress.city
-    temp_shipping['country_code'] = shippingAddress.country
-    temp_shipping['state_code'] = shippingAddress.province
-    temp_shipping['zip'] = shippingAddress.postalCode
+   let temp_shipping = {}
+   temp_shipping['name'] = shippingAddress.fullName
+   temp_shipping['address1'] = shippingAddress.address1
+   temp_shipping['city'] = shippingAddress.city
+   temp_shipping['country_code'] = shippingAddress.country
+   temp_shipping['state_code'] = shippingAddress.province
+   temp_shipping['zip'] = shippingAddress.postalCode
 
-    let elements = []
+   let elements = []
 
-    items_g.map(function (item) {
-        let temp = {}
-        temp['quantity'] = item.quantity
-        temp['variant_id'] = item.id
-        elements.push(temp)
-    })
+   items_g.map(function (item) {
+      let temp = {}
+      temp['quantity'] = item.quantity
+      temp['variant_id'] = item.id
+      elements.push(temp)
+   })
 
-    let send_order = Object.assign({ recipient: temp_shipping, items: elements })
-    console.info("--send_order:", send_order)
-    if ('Test' == method) {
-        // console.info("--shippingAddress:", shippingAddress.address1)
+   let send_order = Object.assign({ recipient: temp_shipping, items: elements })
+   console.info("--send_order:", send_order)
+   if ('Test' == method) {
+      // console.info("--shippingAddress:", shippingAddress.address1)
 
-        resp.type = ''//eg array
-        resp.ispacked = false
-        console.log(resp)
-        request({
-            url: "https://api.printful.com/shipping/rates",
-            headers: {
-                'Authorization': 'Basic ' + config.apiKey,
-            },
-            method: 'POST',
-            json: send_order
+      resp.type = ''//eg array
+      resp.ispacked = false
+      console.log(resp)
+      request({
+         url: "https://api.printful.com/shipping/rates",
+         headers: {
+            'Authorization': 'Basic ' + config.apiKey,
+         },
+         method: 'POST',
+         json: send_order
 
-        }, function (error, response, body) {
-            // console.info("--response:", response)
-            console.info("--body printify-rate:", body)
-            var shippin_rates = {
-                "rates": [{
-                    "cost": 1,
-                    "description": "1$ shipping"
-                }
-                ]
+      }, function (error, response, body) {
+         // console.info("--response:", response)
+         console.info("--body printify-rate:", body)
+         var shippin_rates = {
+            "rates": [{
+               "cost": 1,
+               "description": "1$ shipping"
             }
-            res.json(shippin_rates); //res is the response object, and it passes info back to client-side
-        });
-        // res.json(resp)
-    } else {
-        resp.errorLevel = -1
-        resp.errorMessage = 'mismatch'
-        console.log(resp)
-        res.json(resp)
-    }
+            ]
+         }
+         res.json(shippin_rates); //res is the response object, and it passes info back to client-side
+      });
+      // res.json(resp)
+   } else {
+      resp.errorLevel = -1
+      resp.errorMessage = 'mismatch'
+      console.log(resp)
+      res.json(resp)
+   }
 
 });
 
-app.post('/printify-create-order', (req, res) => {
-    console.info("--res:", res)
-    console.log(req.fields)
-    const user = req.fields.user
-    const pswd = req.fields.pswd // just changed from pswdH
 
-    // const method = req.fields.method
-    const mode = req.fields.mode
-    console.info("--mode:", mode)
-    const params = req.fields
+/*
+* webhook called once the order have been placed and paind with snipcart 
+*
+*/
+app.post('/printful-create-order', (req, res) => {
+   console.info("2)-----printful-create-order:")
+   const user = req.fields.user
+   const pswd = req.fields.pswd // just changed from pswdH
 
-    let shippingAddress = params.content['shippingAddress']
-    console.info("--shippingAddress:", shippingAddress)
-    let items_g = params.content['items']
-    console.info("--items:", items_g)
+   // const method = req.fields.method
+   const mode = req.fields.mode
+   const params = req.fields
 
-    let temp_shipping = {}
-    temp_shipping['name'] = shippingAddress.fullName
-    temp_shipping['address1'] = shippingAddress.address1
-    temp_shipping['city'] = shippingAddress.city
-    temp_shipping['country_code'] = shippingAddress.country
-    temp_shipping['state_code'] = shippingAddress.province
-    temp_shipping['zip'] = shippingAddress.postalCode
+   const resp: any = {} // new response
+   if ('Test' == mode && params.eventName == 'order.completed') {
 
-    let elements = []
+      resp.type = ''//eg array
+      resp.ispacked = false
 
-    items_g.map(function (item) {
-        let temp = {}
-        temp['quantity'] = item.quantity
-        temp['sync_variant_id'] = item.metadata.sync_variant_id
-        elements.push(temp)
-    })
+      let shippingAddress = params.content['shippingAddress']
+      console.log("TCL: params", params)
+      let items_g = params.content['items']
 
-    let send_order = Object.assign({ recipient: temp_shipping, items: elements })
-    console.info("--send_order:", send_order)
+      let temp_shipping = {}
+      temp_shipping['name'] = shippingAddress.fullName
+      temp_shipping['address1'] = shippingAddress.address1
+      temp_shipping['city'] = shippingAddress.city
+      temp_shipping['country_code'] = shippingAddress.country
+      temp_shipping['state_code'] = shippingAddress.province
+      temp_shipping['zip'] = shippingAddress.postalCode
+
+      let elements = []
+
+      items_g.map(function (item) {
+         let temp = {}
+         temp['quantity'] = item.quantity
+         temp['sync_variant_id'] = item.metadata.sync_variant_id
+         elements.push(temp)
+      })
+
+      let send_order = Object.assign({ recipient: temp_shipping, items: elements })
+      console.info("--send_order:", send_order)
 
 
-    const resp: any = {} // new response
-    if ('Test' == mode) {
+      console.log(resp)
 
-        resp.type = ''//eg array
-        resp.ispacked = false
-
-        console.log(resp)
-
-        request({
-            url: "https://api.printful.com/orders",
-            headers: {
-                'Authorization': 'Basic ' + config.apiKey,
-            },
-            method: 'POST',
-            json: send_order
-        }, function (error, response, body) {
-            // console.info("--response:", response)
-            console.info("--body:", body)
-            res.json(body); //res is the response object, and it passes info back to client-side
-        });
-        // res.json(resp)
-    } else {
-        resp.errorLevel = -1
-        resp.errorMessage = 'mismatch'
-        console.log(resp)
-        res.json(resp)
-    }
-    console.info()
+      request({
+         url: "https://api.printful.com/orders",
+         headers: {
+            'Authorization': 'Basic ' + config.apiKey,
+         },
+         method: 'POST',
+         json: send_order
+      }, function (error, response, body) {
+         console.info("--body:", body)
+         res.json(body); //res is the response object, and it passes info back to client-side
+      });
+      // res.json(resp)
+   } else {
+      resp.errorLevel = -1
+      resp.errorMessage = 'mismatch'
+      console.log(resp)
+      res.json(resp)
+   }
+   console.info()
 })
 
 
