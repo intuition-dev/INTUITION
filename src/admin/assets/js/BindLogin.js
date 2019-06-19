fb6170cff5ec483936ee2138c3fe0505a4feccc8
Liza K <liza.kislyakova@gmail.com>
2019-06-19 20:38:44 +0300

    [Intu]: emailjs explanation and redirect after reset to login form add

--- src/admin/assets/js/BindLogin.js
+++ src/admin/assets/js/BindLogin.js
@@ -43,6 +43,10 @@ class BindLogin {
                 .then(function(result) {
                     if (result) {
                         $('.js-pass-changed-notification').removeClass('d-hide');
+                        setTimeout(function() {
+                            $('.js-pass-changed-notification').addClass('d-hide');
+                            $('.js-login-form, .js-btn-login, [data="reset-password"]').removeClass('d-hide');
+                        }, 4000);
                     } else {
                         $('.js-pass-changed-notification-err').removeClass('d-hide');
                     }
