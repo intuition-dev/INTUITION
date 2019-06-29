0eb304be4cb6128a57ba85ae422ecdae2e7e6d74
Vic <uptimevic@gmail.com>
2019-06-28 21:47:01 -0400

    sync

--- XC/xcAPI/index.js
+++ XC/xcAPI/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Serv_1 = require("mbake/lib/Serv");
 const mainEApp = new Serv_1.ExpressRPC();
 mainEApp.makeInstance(['*']);
-mainEApp.appInst.post('/api/editPg', function (req, res) {
+mainEApp.handleRRoute('api', 'editPg', function (req, res) {
     const resp = {};
     resp.result = 'OK';
     res.json(resp);
@@ -13,3 +13,10 @@ mainEApp.serveStatic('../xcApp');
 mainEApp.appInst.listen(8888, () => {
     console.info('server running on port: 8888');
 });
+class Check {
+    auth(user, pswd, resp, ctx) {
+        return new Promise(function (resolve, reject) {
+            resolve('NO');
+        });
+    }
+}
---
+++ XC/xcAPI/lib/DB.ts
@@ -0,0 +1,6 @@
+
+
+import sqlite = require('sqlite')
+const bcrypt = require('bcryptjs') // to hash pswdws
+const fs = require('fs-extra')
+
