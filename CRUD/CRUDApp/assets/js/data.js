a10673a2d3142aee3dd7a683f07254a41aff8136
Vic <uptimevic@gmail.com>
2019-06-29 15:41:21 -0400

    sync

---
+++ CRUD/CRUDApp/assets/js/data.js
@@ -0,0 +1,13 @@
+  
+var tabledata = [
+   {id:2, name:"Mary May", age:"1", col:"blue" },
+   {id:3, name:"Christine Lobowski", age:"42", col:"green"},
+   {id:4, name:"Brendon Philips", age:"125", col:"orange" },
+   {id:5, name:"Margret Marmajuke", age:"16", col:"yellow" },
+]
+
+depp.require('RPC', onPoly)
+
+function onPoly() {
+   console.log('data')
+}
--- CRUD/CRUDApp/assets/js/loader.js
+++ CRUD/CRUDApp/assets/js/loader.js
@@ -1,8 +1,36 @@
 
-depp.require(['bsDefaultStyle', 'tabulatorDefaultStyle', 'FontsLoaded'], function() {
+depp.define({'oops':'https://cdn.jsdelivr.net/npm/tabulator-tables@4.2.7/dist/css/tabulator.css'})
+
+depp.require(['FontsLoaded', 'bsDefaultStyle', 'tabulatorDefaultStyle'], function() {
    console.log('styles')
 }) 
+//'tabulatorDefaultStyle'
 
-depp.require('tabulator', function(){
-   console.log('tab')
-})
+depp.require(['tabulator', 'DOM'], tab1)
+
+function tab1() {
+   
+   var tabledata = [
+      {id:2, name:"Mary May", age:"1", col:"blue" },
+      {id:3, name:"Christine Lobowski", age:"42", col:"green"},
+      {id:4, name:"Brendon Philips", age:"125", col:"orange" },
+      {id:5, name:"Margret Marmajuke", age:"16", col:"yellow" },
+   ]
+
+   var table = new Tabulator('#table1', {
+      data:tabledata, //assign data to table
+      layout:"fitColumns", //fit columns to width of table (optional)
+
+      columns:[ //Define Table Columns
+         {title:"Name", field:"name", width:150},
+         {title:"Age", field:"age", align:"left"},
+         {title:"Favourite Color", field:"col"},
+      ],
+
+      rowClick:function(e, row){ //trigger an alert message when the row is clicked
+         //alert("Row " + row.getData().id + " Clicked!!!!");
+      }
+   })
+   console.log('tab1')
+
+}
