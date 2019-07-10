763ece8524f4d740bb05d303d8aa2ed9995f7bcd
vic <uptimevic@gmail.com>
2019-07-10 19:30:01 -0400

    ok

--- intu/WWW/test/loader.js
+++ intu/WWW/test/loader.js
@@ -1,7 +1,28 @@
 
-// depp.define({'style':'css/bootstrap.css'})
-
 depp.require(['FontsLoaded','bsDefaultStyle'], function() {
    console.log('ready')
 
 }) 
+
+
+depp.define({
+   'TabulatorBind': [
+      '//cdn.jsdelivr.net/npm/qunit@2.9.2/qunit/qunit.css',
+      '//cdn.jsdelivr.net/npm/qunit@2.9.2/qunit/qunit.min.js',
+      '/assets/models/TabulatorViewModel.js',
+      '/assets/models/service/TabulatorService.js',
+      '/assets/models/FormViewModel.js',
+      'TabulatorVMTest.js'
+   ]
+})
+
+depp.require(['pre','TabulatorBind'], setupBinding)
+
+function setupBinding() {
+   console.info('binding', Date.now() - _start)
+
+   var test = new TabulatorVMTest()
+   test.test1()
+   test.test2()
+   test.test3()
+}
