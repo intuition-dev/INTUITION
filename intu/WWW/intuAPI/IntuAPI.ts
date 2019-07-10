f1a1c30a23db61e1dc68099fbf205b265f2942b8
vic <uptimevic@gmail.com>
2019-07-10 19:10:42 -0400

    sync

--- intu/WWW/intuAPI/IntuAPI.ts
+++ intu/WWW/intuAPI/IntuAPI.ts
@@ -4,24 +4,27 @@
  *
  */
 
+ declare let httpRPC:any
 
 class IntuAPI {
 
    serviceRPC
 
    constructor() {
-      console.log("TCL: IntuAPI -> constructor -> apiPort", apiPort)
-      this.serviceRPC = new httpRPC(apiProtocol, apiHost, typeof apiPort != 'undefined' ? apiPort : "");
+      var pro = window.location.protocol;
+      pro = pro.replace(':', '');
+      var host = window.location.hostname;
+      var port = window.location.port;
+
+      console.log("TCL: IntuAPI -> constructor -> apiPort", port)
+
+      this.serviceRPC = new httpRPC(pro, host, port)
       let email = window.sessionStorage.getItem('username');
       let pass = window.sessionStorage.getItem('password');
       if (email !== null && pass !== null) {
          this.serviceRPC.setUser(email, pass);
       }
-   }
-
-   /**
-    ********************* EDITOR APP REQUESTS ************************
-    */
+   }//()
 
    /** editor users authentication
     * @param email user email, eg: 'example@example.com'
@@ -44,11 +47,7 @@ class IntuAPI {
          })
    }
 
-   /**
-    * get list of directories
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
-    */
+
    getDirsList() {
       let email = window.sessionStorage.getItem('username');
       let pass = window.sessionStorage.getItem('password');
@@ -61,8 +60,6 @@ class IntuAPI {
    /**
     * get directories' subdirectories list
     * @param id path to post, eg: 'blog/post-2'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    getSubDirsList(id) {
       let email = window.sessionStorage.getItem('username');
@@ -78,8 +75,6 @@ class IntuAPI {
     * get file content
     * @param id file name, eg: '/title.md'
     * @param pathPrefix path to file, eg: 'blog/post-4'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    getFile(id, pathPrefix) {
       let email = window.sessionStorage.getItem('username');
@@ -97,8 +92,6 @@ class IntuAPI {
     * @param id file name, eg: '/title.md'
     * @param md file content, eg: '###### Lorem ipsum dd dolor sit {.title}'
     * @param pathPrefix path to file, eg: 'blog/post-4'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    saveFile(id, md, pathPrefix) {
       let email = window.sessionStorage.getItem('username');
@@ -117,8 +110,6 @@ class IntuAPI {
     * @param id file name, eg: '/title.md'
     * @param md file content, eg: '###### Lorem ipsum dd dolor sit {.title}'
     * @param pathPrefix path to file, eg: 'blog/post-4'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    mbakeCompile(id, md, pathPrefix) {
       let email = window.sessionStorage.getItem('username');
@@ -140,8 +131,6 @@ class IntuAPI {
     * clone page
     * @param id new page folder name, eg: 'post-cpv'
     * @param pathPrefix path to file, eg: 'blog/post-4'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    clonePage(id, pathPrefix) {
       let email = window.sessionStorage.getItem('username');
@@ -158,8 +147,6 @@ class IntuAPI {
     * file upload
     * @param data FormData
     * @param pathPrefix path to file, eg: 'blog/post-4'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    upload(data, pathPrefix) {
       let email = window.sessionStorage.getItem('username');
@@ -181,8 +168,6 @@ class IntuAPI {
     * set publishDate field to dat.yaml
     * @param publish_date epoch date format, eg: '1602288000'
     * @param pathPrefix post path file, eg: 'blog/post-4'
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
     */
    setPublishDate(publish_date, pathPrefix) {
       let email = window.sessionStorage.getItem('username');
@@ -195,11 +180,7 @@ class IntuAPI {
       });
    }
 
-   /**
-    * get mbake version
-    * @param email user email, eg: 'example@example.com'
-    * @param pass user password, eg: '123456'
-    */
+
    getMbakeVersion() {
       let email = window.sessionStorage.getItem('username');
       let pass = window.sessionStorage.getItem('password');
@@ -237,11 +218,6 @@ class IntuAPI {
       })
    }
 
-
-   /**
-    ********************* ADMIN APP REQUESTS ************************
-    */
-
    /**
     * admin authentication
     * @param email user email, eg: 'example@example.com'
@@ -267,8 +243,6 @@ class IntuAPI {
    /**
     * set up starter application type
     * @param item item, eg: 'blog'
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
     */
    setupApp(item) {
       let admin_email = window.sessionStorage.getItem('username');
@@ -280,11 +254,6 @@ class IntuAPI {
       });
    }
 
-   /**
-    * get data for editors table
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
-    */
    getEditorsList() {
       let admin_email = window.sessionStorage.getItem('username');
       let admin_pass = window.sessionStorage.getItem('password');
@@ -299,9 +268,7 @@ class IntuAPI {
     * add new user
     * @param name new user name, eg: 'Jane Doe'
     * @param email new user email, eg: 'example@example.com'
-    * @param password new user password, eg: 'dfgsdgdsfg' 
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
+    * @param password new user password
     */
    addEditor(name, email, password) {
       let admin_email = window.sessionStorage.getItem('username');
@@ -319,8 +286,6 @@ class IntuAPI {
     * edit user name
     * @param uid user id, eg: 'I3fE7p5NjtV1Y1m5pWBsZlyi4W62'
     * @param name user name, eg: 'Jane Doe'
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
     */
    editEditor(uid, name) {
       return this.serviceRPC.invoke('api', 'admin', 'edit-editor', {
@@ -334,8 +299,6 @@ class IntuAPI {
    /**
     * remove user 
     * @param uid user id, eg: 'I3fE7p5NjtV1Y1m5pWBsZlyi4W62'
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
     */
    deleteEditor(uid) {
       return this.serviceRPC.invoke('api', 'admin', 'delete-editor', {
@@ -370,11 +333,6 @@ class IntuAPI {
       })
    }
 
-   /**
-    * get config files 
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
-    */
    getConfig() {
       console.log('get config start')
       let admin_email = window.sessionStorage.getItem('username');
@@ -386,9 +344,10 @@ class IntuAPI {
    }
 
    /**
-    * update config files 
-    * @param admin_email admin user email, eg: 'example@example.com'
-    * @param admin_pass admin user password, eg: '123456'
+    * 
+    * @param port 
+    * @param path 
+    * @param printfulApi 
     */
    updateConfig(port, path, printfulApi) {
       let admin_email = window.sessionStorage.getItem('username');
@@ -402,8 +361,8 @@ class IntuAPI {
       });
    }
 
-   createConfig(serialised) {
 
+   createConfig(serialised) {
       var email = serialised.filter(email => email.name == 'email')[0].value
       var password = serialised.filter(password => password.name == 'password')[0].value
       var emailjsService_id = serialised.filter(emailjsService_id => emailjsService_id.name == 'service_id')[0].value
@@ -422,6 +381,6 @@ class IntuAPI {
          }).catch((error) => {
             console.info("--error:", error)
          })
-   }
+   }//()
 
-}
+}//class
