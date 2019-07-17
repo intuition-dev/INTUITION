91fa2595bf5691da2ed9211ad1506b4da6a1812c
vic <uptimevic@gmail.com>
2019-07-17 17:31:38 -0400

    slick

--- intu4e/www/layout/ui-loader.js
+++ intu4e/www/layout/ui-loader.js
@@ -1,5 +1,7 @@
 
-depp.require(['FontsLoaded','bsDefaultStyle', 'DOM', 'jquery', 'slickCarousel'], function() {
+depp.define({'st':'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css'})
+
+depp.require(['FontsLoaded','bsDefaultStyle', 'DOM', 'jquery', 'slickCarousel', 'st'], function() {
    console.log('ready')
    depp.done('depps')
    
