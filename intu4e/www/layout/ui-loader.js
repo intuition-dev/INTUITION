bb378a0e545c367ec75a2feb7c7206bfb773bc58
vic <uptimevic@gmail.com>
2019-07-18 14:38:44 -0400

    kick

--- intu4e/www/layout/ui-loader.js
+++ intu4e/www/layout/ui-loader.js
@@ -1,8 +1,22 @@
 
-depp.define({'st':'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css'})
+depp.define({
+   'st':'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css'
+   ,'snipcartDefaultCss':'https://cdn.snipcart.com/themes/v3.0.0-beta.3/default/snipcart.css'
+})
 
-depp.require(['FontsLoaded','bsDefaultStyle', 'DOM', 'jquery', 'slickCarousel', 'st'], function() {
+depp.require(['FontsLoaded','bsDefaultStyle', 'DOM', 'jquery', 'slickCarousel', 'st', 'snipcartDefaultCss'], function() {
    console.log('ready')
    depp.done('depps')
    
 }) 
+
+
+function loadSnipCart(key) {
+   return new Promise(function (resolve, reject) {
+      depp.require('jquery', function(){
+         addScript('https://cdn.snipcart.com/themes/v3.0.0-beta.3/default/snipcart.js', function(){
+            resolve('OK')
+         }, 'data-api-key', key, 'snipcart')
+      })
+   })//pro
+}//()
