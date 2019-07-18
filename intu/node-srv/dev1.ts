332b0bf1892b6e0ba2e69a74ed1e37c3af8c274d
vitalii10 <bondar.vitalii1@gmail.com>
2019-07-18 19:59:24 +0300

    edit test for adb.ts

--- intu/node-srv/dev1.ts
+++ intu/node-srv/dev1.ts
@@ -9,111 +9,112 @@ const adb =new ADB()
 adb.init().then(tst)
 
 async function tst() {
+   let result;
 
-   perfy.start('loop-stuff');
-   logger.trace('getSal:')
+   perfy.start('getSalt', true);
+   logger.trace('getSalt:')
 
    logger.trace(await adb.getSalt())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getSalt')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('monitor', true);
    logger.trace('monitor:')
 
    logger.trace(await adb.monitor())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('monitor')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('setAppPath', true);
    logger.trace(`setAppPath('appPath'):`)
 
    logger.trace(await adb.setAppPath('appPath'))
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('setAppPath')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('getAppPath', true);
    logger.trace('getAppPath:')
 
    logger.trace(await adb.getAppPath())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getAppPath')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('getPort', true);
    logger.trace('getPort:')
 
    logger.trace(await adb.getPort())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getPort')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('getConfig', true);
    logger.trace('getConfig:')
 
    logger.trace(await adb.getConfig())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getConfig')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('getVcodeAdmin', true);
    logger.trace('getVcodeAdmin:')
 
    logger.trace(await adb.getVcodeAdmin())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getVcodeAdmin')
    console.log('', result.time )
 
-   perfy.start('loop-stuff');
+   perfy.start('getVcodeEditor', true);
    logger.trace(`getVcodeEditor('n1@m.com):`)
 
    logger.trace(await adb.getVcodeEditor('n1@m.com'))
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getVcodeEditor')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('addEditor', true);
    var addEditorGuid = uuidv4()
    logger.trace(`addEditor(${addEditorGuid}, 'Editor${addEditorGuid}', 'e@m.com', '1111'):`)
    
    logger.trace(await adb.addEditor(addEditorGuid, 'Editor2'+addEditorGuid, 'e2@m.com', '1111'))
    
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('addEditor')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('getEditors', true);
    logger.trace('getEditors:')
 
    logger.trace(await adb.getEditors())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getEditors')
    console.log('', result.time )
 
-   perfy.start('loop-stuff');
+   perfy.start('deleteEditor', true);
    logger.trace('deleteEditor:')
 
    logger.trace(await adb.deleteEditor(addEditorGuid))
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('deleteEditor')
    console.log('', result.time )
 
 
-   perfy.start('loop-stuff');
+   perfy.start('getEditors2', true);
    logger.trace('getEditors:')
 
    logger.trace(await adb.getEditors())
 
-   var result = perfy.end('loop-stuff')
+   result = perfy.end('getEditors2')
    console.log('', result.time )
 
 }//()
@@ -124,4 +125,4 @@ function uuidv4() {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
-  }
+}
