c7f127033fea64fab9eb88c88576acb91f59a294
vitalii10 <bondar.vitalii1@gmail.com>
2019-07-19 16:55:52 +0300

    [intu] fix AppLogic

--- intu/node-srv/lib/AppLogic.ts
+++ intu/node-srv/lib/AppLogic.ts
@@ -5,12 +5,14 @@ import fs = require('fs-extra')
 
 export class AppLogic {
     
-    autoBake(appPath, itemPath, fileName) {
+    async autoBake(appPath, itemPath, fileName) {
         const meta = new MetaPro(appPath)
-        setTimeout(function(){
+        const res = await meta.autoBake(itemPath, fileName)
+        return res
+        //setTimeout(function(){
             // autobake is automatic in CLI, and tested! So if anything fix the args till it works
-            meta.autoBake(itemPath, fileName)
-        },1)
+           // meta.autoBake(itemPath, fileName)
+        //},1)
     }//()
 
     /**
@@ -26,29 +28,34 @@ export class AppLogic {
         await dat.write()
 
         // update items json or such as needed
-        this.autoBake(appPath, itemPath, 'dat.yaml')
+        const res = await this.autoBake(appPath, itemPath, 'dat.yaml')
+        return res
     }//()
 
     async clone(appPath, item, newItem) {
         const fo = new FileOps(appPath)
-        await fo.clone(item, newItem)
+        const res = await fo.clone(item, newItem)
+        return res
     }
 
     /**
     Silently archive a file to archive directory in the folder 
      */
-    archive(appPath, itemPath, fileName)  {
+    async archive(appPath, itemPath, fileName)  {
 
         const oldPath = appPath  + itemPath
         
-        const targetPath = appPath  + itemPath + '/archive'
+        const targetPath = appPath  + itemPath + '/archive/'
         if(!fs.existsSync)
             fs.mkdirSync(targetPath)
         
         const fo = new FileOps(appPath)
         const count = fo.count(fileName)
 
-        fs.copySync(oldPath+fileName, targetPath+fileName+count)
+        await fs.copySync(oldPath+fileName, targetPath+fileName+count)
+
+        const res = fs.pathExists(oldPath+fileName, targetPath+fileName+count) ? 'OK' : 'Err'
+        return res
     }//()
 
 }//()
