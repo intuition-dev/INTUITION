16023beaecc5e00fce84b4a002b1770a5d134499
vic <uptimevic@gmail.com>
2019-07-20 06:35:18 -0400

    sync

--- intu4s&s/node-srv/lib/SDB.ts
+++
@@ -1,9 +0,0 @@
-
-import { ADB } from 'intu/node-srv/lib/ADB';
-
-export class SDB extends ADB {
-
-    
-}
-
-
---
+++ intu4ss/node-srv/lib/SDB.ts
@@ -0,0 +1,18 @@
+
+import { ADB } from 'intu/node-srv/lib/ADB';
+
+export class SDB extends ADB {
+
+    
+    addTable() {
+
+    }
+
+    getAPIs() { // shipping api and maybe shoping api
+
+    }
+
+}//class
+
+
+
