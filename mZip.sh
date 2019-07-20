f946e016e9c00603229c47e85b850f918c0e04d0
vic <uptimevic@gmail.com>
2019-07-20 09:20:33 -0400

    ok

--- mZip.sh
+++ mZip.sh
@@ -1,10 +1,10 @@
 # make new release of example apps
 
 rm -rf ./CRUD/node_modules
-rm -rf ./intu4ss/node_modules
+rm -rf ./intu4SS/node_modules
 
 rm ./docs/*.zip
 
 zip -r ./docs/CRUD.zip CRUD
-zip -r ./docs/intu4ss.zip intu4ss
+zip -r ./docs/intu4SS.zip intu4SS
 
