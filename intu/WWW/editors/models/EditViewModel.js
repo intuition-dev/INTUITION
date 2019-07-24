e2eb0551980983840385254ebfb654d13732370a
Liza K <liza.kislyakova@gmail.com>
2019-07-24 14:28:26 +0300

    [intu]: removed cached js

--- intu/WWW/editors/models/EditViewModel.js
+++
@@ -1,9 +0,0 @@
-var EditorsViewModel = (function () {
-    function EditorsViewModel() {
-    }
-    EditorsViewModel.prototype.listF = function () { };
-    EditorsViewModel.prototype.openFile = function () { };
-    EditorsViewModel.prototype.saveFile = function () { };
-    EditorsViewModel.prototype.bakeFile = function () { };
-    return EditorsViewModel;
-}());
