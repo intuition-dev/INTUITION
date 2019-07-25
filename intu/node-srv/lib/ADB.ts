197234e1564d8d5cf3f07c73dc6e02258451ede6
vic <uptimevic@gmail.com>
2019-07-25 17:03:54 -0400

    bump

--- intu/node-srv/lib/ADB.ts
+++ intu/node-srv/lib/ADB.ts
@@ -10,7 +10,7 @@ import { iAuth } from 'mbake/lib/Serv'
 export class ADB extends BaseDB { 
 
     static veri() {
-        return '0.17.21'
+        return '0.17.29'
      }
   
    protected static db
