c43a58bec2d44cf15910feeb1e86151768c3cb3f
vic <uptimevic@gmail.com>
2019-07-28 20:07:04 -0400

    .32

--- src/node-srv/lib/ADB.ts
+++ src/node-srv/lib/ADB.ts
@@ -10,7 +10,7 @@ import { iAuth } from 'mbake/lib/Serv'
 export class ADB extends BaseDB { 
 
     static veri() {
-        return 'v0.97.05'
+        return 'v0.97.32'
      }
   
    protected static db
