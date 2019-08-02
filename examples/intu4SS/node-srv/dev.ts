87db891dd3b85a36fd644099dacc7f11663badf3
Liza K <liza.kislyakova@gmail.com>
2019-08-02 19:51:03 +0300

    [intu4SS]: removed unused

--- examples/intu4SS/node-srv/dev.ts
+++ examples/intu4SS/node-srv/dev.ts
@@ -1,19 +1,19 @@
 
 // Bus. Layer test
-import { Ship } from './lib/Ship';
+// import { Ship } from './lib/Ship';
 
-let ship = new Ship(); 
+// let ship = new Ship(); 
 
-let item = {
-    'quantity': '1',
-    'id': '119018290',
-};
-let address = {
-    'fullName': 'Liza Test',
-    'address1': '4558 Cook Hill Road',
-    'city': 'Cheshire',
-    'country': 'US',
-    'province': 'CT',
-    'postalCode': '06410',
-};
-ship.ship([item], address);
+// let item = {
+//     'quantity': '1',
+//     'id': '119018290',
+// };
+// let address = {
+//     'fullName': 'Liza Test',
+//     'address1': '4558 Cook Hill Road',
+//     'city': 'Cheshire',
+//     'country': 'US',
+//     'province': 'CT',
+//     'postalCode': '06410',
+// };
+// ship.ship([item], address);
