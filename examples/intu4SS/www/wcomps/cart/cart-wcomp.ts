cdf4f3a0dc79f9d976c5f81ec48ae946470a7abc
Liza K <liza.kislyakova@gmail.com>
2019-08-02 20:28:12 +0300

    [intu4SS]: cart page styles add

--- examples/intu4SS/www/wcomps/cart/cart-wcomp.ts
+++ examples/intu4SS/www/wcomps/cart/cart-wcomp.ts
@@ -9,21 +9,95 @@ depp.require(['poly-wcomp', 'mustache'], function(){ // inside the require
    var cTemp = document.createElement('template')
    cTemp.innerHTML = `
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/intuition-dev/toolBelt@v1.2.1/bootStrap/css/bootstrapTop.css">
-      <div itemId={{id}}>
-         <span>{{itemData.item.name}}</span>
-         <span>{{size}}</span>
-         <span>{{quantity}}</span>
+      <div class="card d-flex row mb-2" itemId={{id}}>
+         <div class="col-3">
+            <img src={{image}} alt="Card image cap">
+         </div>
+         <div class="card-body col-9">
+            <h5 class="card-title">{{itemData.item.name}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Size: {{size}}</h6>
+            <p class="card-text">Quantity: {{quantity}}</p>
+            <p class="card-text">Price: $\{{cost}}</p>
+            <a href={{url}} class="card-link mt-3">View item</a>
+         </div>
       </div>
+
+      <style>
+         img {
+            width: 200px;
+            max-width: 100%;
+         }
+         .card {
+            flex-direction: row;
+         }
+         .card-link {
+            border-radius: 0px;
+            font-size: 13px;
+            border: 0;
+            display: inline-block;
+            width: auto;
+            text-decoration: none;
+            vertical-align: middle;
+            white-space: nowrap;
+            padding: 13px 20px;
+            border: solid 1px #24242b;
+            text-transform: uppercase;
+            color: #24242b;
+            letter-spacing: .075em;
+            font-weight: 400;
+            transition: all 0.3s cubic-bezier(0.215,0.61,0.355,1);
+            background: transparent;
+            line-height: 1.4;
+            transition: all .3s ease-in-out;
+         }
+      </style>
    `
    var c2Temp = document.createElement('template')
    c2Temp.innerHTML = `
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/intuition-dev/toolBelt@v1.2.1/bootStrap/css/bootstrapTop.css">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/intuition-dev/toolBelt@v1.2.1/bootStrap/css/bootstrap.css">
-      <div itemId={{id}}>
-         <span>{{itemData.item.name}}</span>
-         <span>{{size}}</span>
-         <span>{{quantity}}</span>
+      <div class="card d-flex row mb-2" itemId={{id}}>
+         <div class="col-3">
+            <img src={{image}} alt="Card image cap">
+         </div>
+         <div class="card-body col-9">
+            <h5 class="card-title">{{itemData.item.name}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Size: {{size}}</h6>
+            <p class="card-text">Quantity: {{quantity}}</p>
+            <p class="card-text">Price: $\{{cost}}</p>
+            <a href={{url}} class="card-link mt-3">View item</a>
+         </div>
       </div>
+
+      <style>
+         img {
+            width: 200px;
+            max-width: 100%;
+         }
+         .card {
+            flex-direction: row;
+         }
+         .card-link {
+            border-radius: 0px;
+            font-size: 13px;
+            border: 0;
+            display: inline-block;
+            width: auto;
+            text-decoration: none;
+            vertical-align: middle;
+            white-space: nowrap;
+            padding: 13px 20px;
+            border: solid 1px #24242b;
+            text-transform: uppercase;
+            color: #24242b;
+            letter-spacing: .075em;
+            font-weight: 400;
+            transition: all 0.3s cubic-bezier(0.215,0.61,0.355,1);
+            background: transparent;
+            line-height: 1.4;
+            transition: all .3s ease-in-out;
+         }
+      </style>
    `
    
    window.customElements.define('cart-wcomp', class extends HTMLElement {
@@ -45,7 +119,11 @@ depp.require(['poly-wcomp', 'mustache'], function(){ // inside the require
          if('data'==aName) {
             const THIZ = this
             let data = JSON.parse(newVal)
-            var rendered = Mustache.render(this.tmpl, {id: data.id, quantity: data.quantity, size: data.size, itemData: data.itemData})
+            let url = data.prefix + data.itemData.url
+            let image = url + '/' + data.itemData.image
+            let cost = data.itemData.item.price * data.quantity;
+            console.log('cost ---> ', cost);
+            var rendered = Mustache.render(this.tmpl, {id: data.id, quantity: data.quantity, size: data.size, itemData: data.itemData, url: url, image: image, cost: cost})
             THIZ.sr.innerHTML = rendered     
          }//fi
       }//()
