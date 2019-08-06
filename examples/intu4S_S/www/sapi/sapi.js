436bd80856e284620823131d4c1c4a4f6f315ac1
Liza K <liza.kislyakova@gmail.com>
2019-08-06 19:54:18 +0300

    [intu4SS]: compiled files

--- examples/intu4S_S/www/sapi/sapi.js
+++ examples/intu4S_S/www/sapi/sapi.js
@@ -2,18 +2,9 @@ var Services = (function () {
     function Services() {
         this.serviceRPC = new httpRPC('http', '0.0.0.0', '3000');
     }
-    Services.prototype.getSessionId = function (id, name, description, image, amount, currency, quantity, url) {
-        console.log('getSessionId', JSON.stringify({
-            id: id,
-            name: name,
-            amount: amount,
-            image: image,
-            url: url,
-            description: description,
-            quantity: quantity,
-            currency: currency,
-        }));
-        return this.serviceRPC.invoke("stripe", "get-session", "createSession", { id: id, name: name, description: description, image: image, amount: amount, currency: currency, quantity: quantity, url: url });
+    Services.prototype.getSessionId = function (data) {
+        console.log('getSessionId', JSON.stringify(data));
+        return this.serviceRPC.invoke("stripe", "get-session", "createSession", data);
     };
     return Services;
 }());
