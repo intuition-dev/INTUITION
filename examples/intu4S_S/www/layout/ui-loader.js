201f627d18ea27157e00480699b027232ea6a265
Liza K <liza.kislyakova@gmail.com>
2019-08-07 15:04:08 +0300

    [intu4S_S]: checkout code to cart page moved

--- examples/intu4S_S/www/layout/ui-loader.js
+++ examples/intu4S_S/www/layout/ui-loader.js
@@ -1,14 +1,11 @@
 
-depp.require(['FontsLoaded', 'bsDefaultStyle', 'DOM', 'pre', 'stripe'], function () {
+depp.require(['FontsLoaded', 'bsDefaultStyle', 'DOM', 'pre'], function () {
 
    console.log('ready')
 
-   depp.done('depps')
-   var stripe = Stripe('pk_test_GnYVBEvEsvWrOAtuxETrAFU500y63B4nQK');
-   var services = new Services()
    var quantity = 1
    var size = ''
-
+   
    // We need size and quantity
    $('[data-size]').click(function (ev) {
       console.log("TCL: ev ", ev)
@@ -72,54 +69,6 @@ depp.require(['FontsLoaded', 'bsDefaultStyle', 'DOM', 'pre', 'stripe'], function
          // TODO: wrap in UI
          alert ('Please select size')
       }
-   });
-
-   $('.js-stripe-checkout').on('click', function (ev) {
-      var data = [];
-      JSON.parse($('cart-wcomp').attr('data')).forEach(element => data.push({ 
-         id: element.id,
-         name: element.itemData.item.name,
-         amount: element.itemData.item.price * 100,
-         image: element.itemData.item.url + element.itemData.image,
-         url: element.itemData.item.url,
-         description: element.itemData.title,
-         quantity: element.quantity,
-         currency: 'USD'
-      }));
-      var addressForm = $('#address-form');
-      var address = {
-         name: addressForm.find('input[name="name"]').val(),
-         company: addressForm.find('input[name="company"]').val(),
-         address1: addressForm.find('input[name="address1"]').val(),
-         address2: addressForm.find('input[name="address2"]').val(),
-         city: addressForm.find('input[name="city"]').val(),
-         state_code: addressForm.find('input[name="state_code"]').val(),
-         state_name: addressForm.find('input[name="state_name"]').val(),
-         country_code: addressForm.find('input[name="country_code"]').val(),
-         country_name: addressForm.find('input[name="country_name"]').val(),
-         zip: addressForm.find('input[name="zip"]').val(),
-         phone: addressForm.find('input[name="phone"]').val(),
-         email:addressForm.find('input[name="email"]').val(),
-      };
-      console.log('CHECKOUT', data, address);
-
-      services.getSessionId(data, address)
-         .then(function (session) {
-            console.log("TCL: sessionId", session)
-            stripe.redirectToCheckout({
-               sessionId: session.id
-            }).then(function (result) {
-               console.log("TCL: result", result.error.message)
-               // If `redirectToCheckout` fails due to a browser or network
-               // error, display the localized error message to your customer
-               // using `result.error.message`.
-            });
-         })
-
-      // if (size.length) {
-      //    // TODO
-      // }
-
    });
 
 });
