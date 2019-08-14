a46e5c2730bbf72438f6018374e628a9170c478c
Liza K <liza.kislyakova@gmail.com>
2019-08-14 17:22:45 +0300

    [Intu]: rename comps

--- src/WWW/admin/settings/BindSettings.js
+++ src/WWW/admin/settings/BindSettings.js
@@ -18,7 +18,7 @@ class BindSettings {
         var _this = this
         this.settingsViewModel.getConfig()
             .then(function(result) {
-                riot.mount('settings-custEl', {
+                riot.mount('settings-comp', {
                     emailjsService_id: result.emailjsService_id,
                     emailjsTemplate_id: result.emailjsTemplate_id,
                     emailjsUser_id: result.emailjsUser_id,
@@ -82,7 +82,7 @@ class BindSettings {
                     window.location.href = "/admin"
                 }
                 //update form
-                riot.mount('settings-custEl', {
+                riot.mount('settings-comp', {
                     pathToSite: result[0].path,
                     port: result[0].port,
                     bindSetup: this,
