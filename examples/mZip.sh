c5ca63c24d2f02d73b1a8f361d94e1c83da4f238
vic <uptimevic@gmail.com>
2019-08-18 16:37:34 -0400

    bump

--- examples/mZip.sh
+++ examples/mZip.sh
@@ -1,9 +1,9 @@
 # make new release of example apps
 rm *.zip
+rm *.sqlite
 
 rm -rf ./examples/CRUD/node_modules
 rm -rf ./examples/intu4S_S/node_modules
 
 zip -r ./CRUD.zip ./examples/CRUD
 zip -r ./intu4SS.zip ./examples/intu4S_S
-
