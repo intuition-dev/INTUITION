74c8717c87dc347e212487e7968ede1ebc3dafb0
vic <uptimevic@gmail.com>
2019-10-01 19:08:50 -0400

    setup

--- examples/CMS/blog/assets/js/ui.js
+++
@@ -1,8 +0,0 @@
-// header menu links highlight
-if ($('[data-js-menu-highlight="about"]').length !== 0) {
-    $('header a.about').addClass('active');
-} else if ($('[data-js-menu-highlight="blog"]').length !== 0) {
-    $('header a.blog').addClass('active');
-} else if ($('[data-js-menu-highlight="forum"]').length !== 0) {
-    $('header a.forum').addClass('active');
-}
--- examples/CMS/www0/assets/spectre/README.md
+++
@@ -1,7 +0,0 @@
-Orig 18/12/29
-
-IMPORTANT: 
-
-Typography section comments out lists
-
-Also open sans is default font in variables ~ line 41
