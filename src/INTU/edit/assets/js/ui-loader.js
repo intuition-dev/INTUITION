902ba06508278fdf8de0cce7b9f2ea53e4866ad1
vic <uptimevic@gmail.com>
2019-11-06 09:18:33 -0500

    ok

--- src/INTU/edit/assets/js/ui-loader.js
+++ src/INTU/edit/assets/js/ui-loader.js
@@ -2,16 +2,16 @@
 depp.define({
     'scripts': ['#jquery', '#DOM'],
 
-    'pop-custelX': ['https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v2.10.18/custel/pop/custel/pop-custel.js'],
+    'pop-custelX': ['https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v2.11.1/custel/pop/custel/pop-custel.js'],
 
     'ui': ['#scripts', '#zebraDate', '#pop-custelX'],
 
     'codeEdit': ['#codemirror', '#scripts', '//cdn.jsdelivr.net/npm/codemirror@5.48.4/keymap/sublime.js'],
     'cssBot': '/edit/assets/css/spectreBottom.css',
 
-    'httpRPC': ['https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v2.10.18/http-rpc+/web/httpRPC.js'],
+    'httpRPC': ['https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v2.11.1/http-rpc+/web/httpRPC.js'],
     'intuAPI': ['/intuAPI/IntuAPI.js'],
-    'baseVM': ['https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v2.10.18/http-rpc+/web/httpRPC.js', '#intuAPI', '/edit/assets/models/BaseViewModel.js',],
+    'baseVM': ['https://cdn.jsdelivr.net/gh/intuition-dev/mbToolBelt@v2.11.1/http-rpc+/web/httpRPC.js', '#intuAPI', '/edit/assets/models/BaseViewModel.js',],
     'loginViewModel': ['#baseVM', '/edit/assets/models/LoginViewModel.js'],
     'editViewModel': ['#baseVM', '/edit/assets/models/EditViewModel.js'],
 
