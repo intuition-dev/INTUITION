bd26ed556d4f0f9ab97ce100be5c7851cc6ab29f
Vic <vic@eml.cc>
2020-02-17 18:33:54 -0500

    bump

--- src/node-srv/lib/BusLogic.ts
+++ src/node-srv/lib/BusLogic.ts
@@ -21,7 +21,7 @@ export class Util {
 export class BusLogic {
 
     static veri() {
-        return 'v2.14.10'
+        return 'v2.14.11'
     }
     async autoBake(appPath, itemPath, fileName) {
         const meta = new MetaPro(appPath)
