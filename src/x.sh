478f54ba8509fc4ef5a8dcea4f0e1d26da3e6758
Vic <vic@eml.cc>
2020-04-05 15:52:54 -0400

    d

--- src/x.sh
+++ src/x.sh
@@ -2,13 +2,4 @@
 clear
 tsc
 
- node mbake.js -w /home/vic/Documents/GitHub/intuDesignWork/src/template
-
-# /home/vic/Documents/GitHub/mbToolBelt/src/vendors/defEventFlux
-
-#  node mbake.js -i /Users/code/Documents/GitHub/intuDocs/5625/items
-
-#  node mbake.js --pug # -t /Users/code/Documents/GitHub/alan/al-prod
-
-#  node mbakex.js -w /Users/code/Documents/GitHub/metaCake/metaCake/webC
-
+node mbake.js -c /home/vic/Downloads
